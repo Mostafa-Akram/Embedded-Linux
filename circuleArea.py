cc1c0b310e6b6a5d82a2660ec34bc155c57edcab
Mostafa Akram <88909585+Mostafa-Akram@users.noreply.github.com>
2024-06-30 18:59:21 +0300

    task edited

--- circuleArea.py
+++ circuleArea.py
@@ -1,5 +1,5 @@
 pi = 3.14158
 
-radius = int(input("Enter the radius of the circle: "))
+radius = float(input("Enter the radius of the circle: "))
 
 print("The area of the circule is: ",str(pi*(radius*radius)) )
